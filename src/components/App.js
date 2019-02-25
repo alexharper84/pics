add30ad8144db6399821157a494083052f2cd019
alexharper84 <alexharper84@gmail.com>
2019-02-25 02:29:59 -0800

    log results

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID f959ca3414c6398b8db535a296a4ac82227405aa50f1edea01decb1264834d1b"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID f959ca3414c6398b8db535a296a4ac82227405aa50f1edea01decb1264834d1b"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
